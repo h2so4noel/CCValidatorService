1420eef9a90956c272c1469608f0480f6e085982
h2so4noel <h2so4noel@gmail.com>
2014-11-04 20:09:18 +0700

    edit unused import

--- src/view/MainUI.java
+++ src/view/MainUI.java
@@ -4,7 +4,6 @@ import java.awt.Container;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.net.URL;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
